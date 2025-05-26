378440df8011ec6becf0119912c69fbaedc2478e
sunflowerthomas <t.man.sunflower@gmail.com>
2025-05-26 21:29:22 +0100

    rewrote locomotion scripts in c#, broken, will fix tmrw.

---
+++ Assets/locomotion.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class locomotion : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // Horizontal input
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1f); // Optional: prevents faster diagonal movement
+
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+        }
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Combine horizontal and vertical movement
+        Vector3 finalMove = (move * playerSpeed) + (playerVelocity.y * Vector3.up);
+        controller.Move(finalMove * Time.deltaTime);
+    }
+}
